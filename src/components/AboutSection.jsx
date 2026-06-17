bb803a8d7b92d735fd3e2199b0b8a2001ce50cc6
Rakshit052005 <rakshitgarg052005@gmail.com>
2026-06-17 14:10:22 +0530

    Portfolio updated

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -35,7 +35,7 @@ export const AboutSection = () => {
                                 Get in Touch
                             </a>
 
-                            <a href="CV_Rakshit.pdf" download className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300">
+                            <a href="Rakshit_CV.pdf" download className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300">
                                 Download CV
                             </a>
 
